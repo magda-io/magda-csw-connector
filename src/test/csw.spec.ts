559bded5deb8568ee4300640d53d4ebec9855163
Jacky Jiang <t83714@gmail.com>
2021-11-04 21:50:27 +1100

    add test cases for post & basic auth

--- src/test/csw.spec.ts
+++ src/test/csw.spec.ts
@@ -1,5 +1,7 @@
 import nock from "nock";
 
+import fs from "fs";
+
 import {
     JsonConnector,
     AuthorizedRegistryClient as Registry,
@@ -8,7 +10,7 @@ import {
 import sinon from "sinon";
 import { expect } from "chai";
 
-import Csw from "../Csw";
+import Csw, { CswOptions } from "../Csw";
 import createTransformer from "../createTransformer";
 import datasetAspectBuilders from "../datasetAspectBuilders";
 import distributionAspectBuilders from "../distributionAspectBuilders";
@@ -41,12 +43,13 @@ describe("csw connector", () => {
         (console.warn as any).restore();
     });
 
-    beforeEach(() => {
+    function setupConnector(options: Partial<CswOptions> = {}) {
         const csw = new Csw({
             id: ID,
             baseUrl: BASE_CSW_URL,
             name: ID,
-            pageSize: PAGE_SIZE
+            pageSize: PAGE_SIZE,
+            ...options
         });
 
         const registry = new Registry({
@@ -79,7 +82,7 @@ describe("csw connector", () => {
 
         cswScope = nock(BASE_CSW_URL);
         registryScope = nock(REGISTRY_URL);
-    });
+    }
 
     afterEach(() => {
         nock.cleanAll();
@@ -88,6 +91,8 @@ describe("csw connector", () => {
     // --- aurin response is a sample xml response carries valid ID
     // --- however, previous xml process logic will trigger xml parse error as namespaces are set on root element instead of <gmd:MD_Metadata>
     it("should parse aurin response without `put record with no id` error", async () => {
+        setupConnector();
+
         cswScope
             .get(/.*/)
             .query((query: any) => query.startPosition === "1")
@@ -117,6 +122,8 @@ describe("csw connector", () => {
     // --- qspatial response is a sample xml response with missing ID
     // --- it will not trigger any xml parse error as namespace are set on <gmd:MD_Metadata>
     it("should parse qspatial response with missing ids without crashing", async () => {
+        setupConnector();
+
         cswScope
             .get(
                 "/?service=CSW&version=2.0.2&request=GetRecords&constraintLanguage=FILTER&constraint_language_version=1.1.0&resultType=results&elementsetname=full&outputschema=http%3A%2F%2Fwww.isotc211.org%2F2005%2Fgmd&typeNames=gmd%3AMD_Metadata&startPosition=1&maxRecords=100"
@@ -131,6 +138,129 @@ describe("csw connector", () => {
         registryScope.delete(/.*/).reply(200, { count: 2 });
 
         const results = await connector.run();
+        const errors = results.datasetFailures.filter(
+            err =>
+                err.error.message.indexOf("Tried to put record with no id:") !==
+                -1
+        );
+        // --- there should ONLY be `no id` error
+        expect(errors.length).to.equal(results.datasetFailures.length);
+    }).timeout(30000);
+
+    it("should send POST request to getRecords endpoints when `usePostRequest` = true", async () => {
+        const outputSchema = "http://standards.iso.org/iso/19115/-3/mdb/2.0";
+        const typeNames = "gmd:MD_Metadata";
+        let requestBodyXml = "";
+
+        setupConnector({ usePostRequest: true, outputSchema, typeNames });
+
+        cswScope.post("/").reply(200, (uri, requestBody) => {
+            requestBodyXml = requestBody.toString();
+            return fs.readFileSync(require.resolve("./csw1.xml"));
+        });
+
+        registryScope
+            .put(/.*/)
+            .times(100000)
+            .reply(200);
+
+        registryScope.delete(/.*/).reply(200, { count: 2 });
+
+        const results = await connector.run();
+        const errors = results.datasetFailures.filter(
+            err =>
+                err.error.message.indexOf("Tried to put record with no id:") !==
+                -1
+        );
+        // --- there should ONLY be `no id` error
+        expect(errors.length).to.equal(results.datasetFailures.length);
+
+        expect(requestBodyXml.replace(/\n\s*/g, "")).to.be.equal(
+            `<?xml version="1.0"?>
+        <csw:GetRecords xmlns:csw="http://www.opengis.net/cat/csw/2.0.2"
+                        xmlns:gmd="http://www.isotc211.org/2005/gmd"
+                        service="CSW" version="2.0.2"
+                        outputSchema="http://standards.iso.org/iso/19115/-3/mdb/2.0"
+                        resultType="results"
+                        startPosition="1"
+                        maxRecords="100">
+            <csw:Query typeNames="gmd:MD_Metadata">
+            <csw:Constraint version="1.1.0">
+                <Filter xmlns="http://www.opengis.net/ogc"/>
+            </csw:Constraint>
+            </csw:Query>
+        </csw:GetRecords>`.replace(/\n\s*/g, "")
+        );
+    }).timeout(30000);
+
+    it("should send basic auth headers for POST request when basicAuth info is provided", async () => {
+        const username = "test-username-" + Math.random();
+        const password = "test-password-" + Math.random();
+
+        setupConnector({
+            usePostRequest: true,
+            basicAuth: {
+                username,
+                password
+            },
+            maxRetries: 1
+        });
+
+        cswScope
+            .post("/")
+            .basicAuth({ user: username, pass: password })
+            .replyWithFile(200, require.resolve("./csw1.xml"));
+
+        registryScope
+            .put(/.*/)
+            .times(100000)
+            .reply(200);
+
+        registryScope.delete(/.*/).reply(200, { count: 2 });
+
+        const results = await connector.run();
+
+        expect(cswScope.isDone()).to.be.true;
+
+        const errors = results.datasetFailures.filter(
+            err =>
+                err.error.message.indexOf("Tried to put record with no id:") !==
+                -1
+        );
+        // --- there should ONLY be `no id` error
+        expect(errors.length).to.equal(results.datasetFailures.length);
+    }).timeout(30000);
+
+    it("should send basic auth headers for GET request when basicAuth info is provided", async () => {
+        const username = "test-username-" + Math.random();
+        const password = "test-password-" + Math.random();
+
+        setupConnector({
+            basicAuth: {
+                username,
+                password
+            },
+            maxRetries: 1
+        });
+
+        cswScope
+            .get(
+                "/?service=CSW&version=2.0.2&request=GetRecords&constraintLanguage=FILTER&constraint_language_version=1.1.0&resultType=results&elementsetname=full&outputschema=http%3A%2F%2Fwww.isotc211.org%2F2005%2Fgmd&typeNames=gmd%3AMD_Metadata&startPosition=1&maxRecords=100"
+            )
+            .basicAuth({ user: username, pass: password })
+            .replyWithFile(200, require.resolve("./csw1.xml"));
+
+        registryScope
+            .put(/.*/)
+            .times(100000)
+            .reply(200);
+
+        registryScope.delete(/.*/).reply(200, { count: 2 });
+
+        const results = await connector.run();
+
+        expect(cswScope.isDone()).to.be.true;
+
         const errors = results.datasetFailures.filter(
             err =>
                 err.error.message.indexOf("Tried to put record with no id:") !==
